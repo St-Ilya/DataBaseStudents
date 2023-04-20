a8c29b854b69080ff0139e405eb5d00b7fbbe515
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-04-20 19:13:22 +0300

    Add class Group

---
+++ app/src/main/java/com/st_ilya/databasestudentsmireaproject/Group.java
@@ -0,0 +1,40 @@
+package com.st_ilya.databasestudentsmireaproject;
+
+public class Group {
+
+    private int numberGroup;
+    private String nameGroup;
+
+    public Group(int numberGroup, String nameGroup) {
+        this.numberGroup = numberGroup;
+        this.nameGroup = nameGroup;
+    }
+
+    public int getNumberGroup() {
+        return numberGroup;
+    }
+
+    public void setNumberGroup(int numberGroup) {
+        this.numberGroup = numberGroup;
+    }
+
+    public String getNameGroup() {
+        return nameGroup;
+    }
+
+    public void setNameGroup(String nameGroup) {
+        this.nameGroup = nameGroup;
+    }
+
+    public void newGroup(String nameGroup, int numberGroup) {
+
+    }
+
+    public void refactorGroup() {
+
+    }
+
+    public void deleteGroup() {
+
+    }
+}
