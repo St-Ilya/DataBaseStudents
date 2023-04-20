63d45c93eb15ce084d6ac8162dcb698d23afa8a9
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-04-20 22:48:17 +0300

    setting button ToDo Navigation

---
+++ app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/LoginRegisterFragment.java
@@ -0,0 +1,82 @@
+package com.st_ilya.databasestudentsmireaproject.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.st_ilya.databasestudentsmireaproject.R;
+import com.st_ilya.databasestudentsmireaproject.viewmodel.LoginRegisterViewModel;
+
+public class LoginRegisterFragment extends Fragment {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private Button registerButton;
+
+    private LoginRegisterViewModel loginRegisterViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        loginRegisterViewModel = new ViewModelProvider(this).get(LoginRegisterViewModel.class);
+        loginRegisterViewModel.getUserMutableLiveData().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    //ToDo Navigation
+                }
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        emailEditText = view.findViewById(R.id.fragment_loginregister_email);
+        passwordEditText = view.findViewById(R.id.fragment_loginregister_password);
+        loginButton = view.findViewById(R.id.fragment_loginregister_login);
+        registerButton = view.findViewById(R.id.fragment_loginregister_register);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (email.length() > 0 && password.length() > 0) {
+                    loginRegisterViewModel.register(email, password);
+                }
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (email.length() > 0 && password.length() > 0) {
+                    loginRegisterViewModel.login(email, password);
+                }
+            }
+        });
+
+        return view;
+    }
+}
