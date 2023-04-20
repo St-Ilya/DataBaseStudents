d6f00cd74de6eba1d2f7337cebe9feab2bcdf0b5
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-04-20 22:52:23 +0300

    add two page fragment, ToDo nav

---
+++ app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/LoggedInFragment.java
@@ -0,0 +1,67 @@
+package com.st_ilya.databasestudentsmireaproject.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.st_ilya.databasestudentsmireaproject.R;
+import com.st_ilya.databasestudentsmireaproject.viewmodel.LoggedInViewModel;
+
+public class LoggedInFragment extends Fragment {
+
+    private TextView loggedInUserTextView;
+    private Button logOutButton;
+    private LoggedInViewModel loggedInViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        loggedInViewModel = new ViewModelProvider(this).get(LoggedInViewModel.class);
+        loggedInViewModel.getUserMutableLiveData().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    loggedInUserTextView.setText("Logged In User: " + firebaseUser.getEmail());
+                }
+            }
+        });
+
+        loggedInViewModel.getLoggedOutMutableLiveData().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean loggedOut) {
+                if (loggedOut) {
+                    //ToDO navGraph
+                }
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_loggedinfragment, container, false);
+
+        loggedInUserTextView = view.findViewById(R.id.fragment_loggedin_loggedInUser);
+        logOutButton = view.findViewById(R.id.fragment_loggedin_logOut);
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loggedInViewModel.logOut();
+            }
+        });
+
+        return view;
+    }
+}
