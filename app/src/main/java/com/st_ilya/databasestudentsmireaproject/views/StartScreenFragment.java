29b3285551289fa03ecaa29a7ea980a464f46b8d
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-05-11 18:13:37 +0300

    it is necessary to solve the errors with the transition and with the starting fragment

---
+++ app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/StartScreenFragment.java
@@ -0,0 +1,69 @@
+package com.st_ilya.databasestudentsmireaproject.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.st_ilya.databasestudentsmireaproject.R;
+import com.st_ilya.databasestudentsmireaproject.viewmodel.StartScreenViewModel;
+
+public class StartScreenFragment extends Fragment {
+
+    private Button signInButton;
+    private Button signUpButton;
+
+    private StartScreenViewModel startScreenViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startScreenViewModel = new ViewModelProvider(this).get(StartScreenViewModel.class);
+        startScreenViewModel.getUserMutableLiveData().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    Navigation.findNavController(getView()).navigate(R.id.action_loginRegisterFragment_to_loggedInFragment);
+                }
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_start_screen, container, false);
+
+        signInButton = view.findViewById(R.id.signInButton);
+        signUpButton = view.findViewById(R.id.signUpButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+
+}
