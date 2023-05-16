dd12ab549c2951301d7c50af423de391f83900ed
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-05-16 18:22:07 +0300

    add fields

--- app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/RegisterFragment.java
+++ app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/RegisterFragment.java
@@ -1,7 +1,26 @@
 package com.st_ilya.databasestudentsmireaproject.views;
 
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
 
+import com.st_ilya.databasestudentsmireaproject.viewmodel.RegisterViewModel;
+
 public class RegisterFragment extends Fragment {
+
+    private EditText emailAddress;
+    private EditText name;
+    private EditText surname;
+    private EditText middleName;
+    private EditText dateOfBirth;
+    private EditText groupNumber;
+    private EditText faculty;
+    private EditText password;
+
+    private Button registerButton;
+
+    private RegisterViewModel registerViewModel;
+
 
 }
