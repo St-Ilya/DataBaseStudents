be77d7e1454da7bcac8d3095dfa7958900c4f587
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-05-18 15:36:10 +0300

    fun register update, add map for userID and registerForm

--- app/src/main/java/com/st_ilya/databasestudentsmireaproject/model/AppRepository.java
+++ app/src/main/java/com/st_ilya/databasestudentsmireaproject/model/AppRepository.java
@@ -1,23 +1,44 @@
 package com.st_ilya.databasestudentsmireaproject.model;
 
 import android.app.Application;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.st_ilya.databasestudentsmireaproject.views.RegisterFragment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AppRepository {
 
+    public static final String TAG = "TAG";
     private Application application;
     private FirebaseAuth firebaseAuth;
     private MutableLiveData<FirebaseUser> userMutableLiveData;
     private MutableLiveData<Boolean> loggedOutMutableLiveData;
+    private FirebaseFirestore fStore;
+    private String userID;
+
+    private String emailAddressLogin;
+    private String name;
+    private String surname;
+    private String middleName;
+    private String dateOfBirth;
+    private String groupNumber;
+    private String faculty;
+    private String passwordd;
 
     public AppRepository(Application application) {
         this.application = application;
@@ -37,6 +58,26 @@ public class AppRepository {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            userID = firebaseAuth.getCurrentUser().getUid();
+                            fStore = FirebaseFirestore.getInstance();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("email", emailAddressLogin);
+                            user.put("name", name);
+                            user.put("surname", surname);
+                            user.put("middleName", middleName);
+                            user.put("dateOfBirth", dateOfBirth);
+                            user.put("groupNumber", groupNumber);
+                            user.put("faculty", faculty);
+                            user.put("password", passwordd);
+
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "onSuccess: user Profile is created for " + userID);
+                                }
+                            });
+
                             userMutableLiveData.postValue(firebaseAuth.getCurrentUser());
                         } else {
                             Toast.makeText(application, "Register Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
