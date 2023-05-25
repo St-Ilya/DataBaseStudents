4281f4ed3cf92304e9a409058ce33ade3a775f19
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-05-25 17:50:20 +0300

    rename fields

--- app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/RegisterFragment.java
+++ app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/RegisterFragment.java
@@ -11,24 +11,22 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
-import com.google.firebase.auth.FirebaseUser;
 import com.st_ilya.databasestudentsmireaproject.R;
 import com.st_ilya.databasestudentsmireaproject.viewmodel.RegisterViewModel;
 
 public class RegisterFragment extends Fragment {
 
-    private EditText emailAddressLogin;
-    private EditText name;
-    private EditText surname;
-    private EditText middleName;
-    private EditText dateOfBirth;
-    private EditText groupNumber;
-    private EditText faculty;
-    private EditText password;
+    private EditText emailAddressLoginEdit;
+    private EditText nameEdit;
+    private EditText surnameEdit;
+    private EditText middleNameEdit;
+    private EditText dateOfBirthEdit;
+    private EditText groupNumberEdit;
+    private EditText facultyEdit;
+    private EditText passwordEdit;
 
     private Button registerButton;
 
@@ -39,12 +37,9 @@ public class RegisterFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         registerViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
-        registerViewModel.getUserMutableLiveData().observe(this, new Observer<FirebaseUser>() {
-            @Override
-            public void onChanged(FirebaseUser firebaseUser) {
-                if (firebaseUser != null) {
-                    Navigation.findNavController(getView()).navigate(R.id.action_registerFragment_to_loginRegisterFragment);
-                }
+        registerViewModel.getUserMutableLiveData().observe(this, firebaseUser -> {
+            if (firebaseUser != null) {
+                Navigation.findNavController(getView()).navigate(R.id.action_registerFragment_to_loginRegisterFragment);
             }
         });
 
@@ -55,27 +50,27 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        emailAddressLogin = view.findViewById(R.id.editTextTextEmailAddress);
-        name = view.findViewById(R.id.nameTextEditText);
-        surname = view.findViewById(R.id.surnameTextEditText);
-        middleName = view.findViewById(R.id.middleNameTextEditText);
-        dateOfBirth = view.findViewById(R.id.dateOfBirthTextEditText);
-        groupNumber = view.findViewById(R.id.groupNumberTextEditText);
-        faculty = view.findViewById(R.id.facultyTextEditText);
-        password = view.findViewById(R.id.passwordTextEditText);
+        emailAddressLoginEdit = view.findViewById(R.id.editTextTextEmailAddress);
+        nameEdit = view.findViewById(R.id.nameTextEditText);
+        surnameEdit = view.findViewById(R.id.surnameTextEditText);
+        middleNameEdit = view.findViewById(R.id.middleNameTextEditText);
+        dateOfBirthEdit = view.findViewById(R.id.dateOfBirthTextEditText);
+        groupNumberEdit = view.findViewById(R.id.groupNumberTextEditText);
+        facultyEdit = view.findViewById(R.id.facultyTextEditText);
+        passwordEdit = view.findViewById(R.id.passwordTextEditText);
 
         registerButton = view.findViewById(R.id.buttonRegister);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailAddressLogin.getText().toString();
-                String name = emailAddressLogin.getText().toString();
-                String surname = emailAddressLogin.getText().toString();
-                String middle = emailAddressLogin.getText().toString();
-                String date = emailAddressLogin.getText().toString();
-                String group = groupNumber.getText().toString();
-                String faculty = emailAddressLogin.getText().toString();
-                String password = emailAddressLogin.getText().toString();
+                String email = emailAddressLoginEdit.getText().toString();
+                String name = nameEdit.getText().toString();
+                String surname = surnameEdit.getText().toString();
+                String middle = middleNameEdit.getText().toString();
+                String date = dateOfBirthEdit.getText().toString();
+                String group = groupNumberEdit.getText().toString();
+                String faculty = facultyEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
 
                 if (email.length() > 0 && password.length() > 0) {
                     registerViewModel.register(email, name, surname, middle, date, group, faculty, password);
