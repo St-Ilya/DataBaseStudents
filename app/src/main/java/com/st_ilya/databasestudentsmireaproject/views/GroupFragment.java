b05ed58172a812aefe5461054ca204bc2912657a
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-06-09 20:13:34 +0300

    GroupFragment.java extends Fragment

--- app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/GroupFragment.java
+++ app/src/main/java/com/st_ilya/databasestudentsmireaproject/views/GroupFragment.java
@@ -1,7 +1,12 @@
 package com.st_ilya.databasestudentsmireaproject.views;
 
-import androidx.fragment.app.Fragment;
+import android.view.View;
+import android.widget.AbsListView;
 
-public class GroupFragment extends Fragment {
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GroupFragment extends Fragment{
+
 
 }
